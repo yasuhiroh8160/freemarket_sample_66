bc804a6332e10bd9fce667f70ca5517e90622f7b
yasuhiroh8160 <uriochan@icloud.com>
2020-01-09 20:47:54 +0900

    s3_setting

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,25 @@
 class TestsController < ApplicationController
   def index
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to root_url, notice: 'Add Test'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def test_params
+    params.fetch(:test, {}).permit(:name, :image)
   end
 end
