ed8020877146ee285ecb8c1abe70b85aff2789f5
yasuhiroh8160 <uriochan@icloud.com>
2020-01-15 20:57:59 +0900

    show_detail_top

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'products#index'
+  resources :products, only: [:index, :show]
   resources :tests, only: [:index, :new, :create]
   get '/tmp', to: 'tmps#index'
   get '/logout', to: 'logouts#index'
